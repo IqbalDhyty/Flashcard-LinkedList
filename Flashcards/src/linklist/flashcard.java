39ea1312aa22fc82b02fda16239cb70ca091d495
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-09-13 11:19:09 +0700

    first commit

---
+++ Flashcards/src/linklist/flashcard.java
@@ -0,0 +1,16 @@
+package linklist;
+
+// Class untuk node linkedlist pada program utama
+// Mengandung 
+public class flashcard {
+    private String Kata;
+    private String Definisi;
+
+    public String getKata(){
+        return Kata;
+    }
+
+    public String getDefinisi(){
+        return Definisi;
+    }
+}
