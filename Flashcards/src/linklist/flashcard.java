279850874f6012593d85d63a067710722bf778a2
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-09-13 13:58:09 +0700

    Created method to initialize cards in the begginning

--- Flashcards/src/linklist/flashcard.java
+++ Flashcards/src/linklist/flashcard.java
@@ -46,13 +46,43 @@ public class flashcard {
         this.flashcardSetelah = flashcardSetelah;
     }
 
+    public flashcard(String Kata, String Definisi) {
+        this.Kata = Kata;
+        this.Definisi = Definisi;
+        this.flashcardSebelum = null;
+        this.flashcardSetelah = null;
+    }
+
     /**
      * Method untuk menginisialisasi kartu-kartu saat awal program bermulai
      * @param Kata Array Mengandung kata
      * @param Definisi Array mengandung definsi untuk kata tersebut
-     * @param Awal 
+     * @param Awal variabel node awal dari linked list
+     * @param Akhir variabel node akhir dari linked list
      */
-    public void inisialisasiKartuAwal(String[] Kata, String[] Definisi, flashcard Awal, flashcard Akhir) {
+    public static flashcard inisialisasiKartuAwal(String[] Kata, String[] Definisi, flashcard Awal, flashcard Akhir) {
+        // Loop melalui satu array
+        for (int i = 0; i < Kata.length; i++) {
 
+            // Buat node Baru menggunakan komponen Array
+            flashcard nodeBaru = new flashcard(Kata[i], Definisi[i]);
+
+            // Untuk Node Pertama
+            if (Awal == null){
+                Awal = nodeBaru;
+                Akhir = nodeBaru;
+                // Awal menunjuk ke node sendiri
+                Awal.setflashcardSetelah(Awal);
+                Awal.setflashcardSebelum(Awal);
+            } else {
+                // Untuk Node selanjutnya
+                nodeBaru.setflashcardSebelum(Akhir); // flashcardSebelum Node baru menunjuk ke Akhir lama
+                nodeBaru.setflashcardSetelah(Awal);  // flashcardSetelah menunjuk ke Awal
+                Akhir.setflashcardSetelah(nodeBaru); // flashcardSetelah Akhir lama menunjuk ke ndde baru
+                Awal.setflashcardSebelum(nodeBaru);  // flashcardSebelum Kepada menunjuk ke Akhir baru
+                Akhir = nodeBaru;                    // Akhir menjadi node baru   
+            }
+        }
+        return Awal;
     }
 }
