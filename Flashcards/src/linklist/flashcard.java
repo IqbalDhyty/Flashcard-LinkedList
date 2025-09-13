6357988255b41c18327f32c82f7d1a61c91b3f97
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-09-13 13:27:29 +0700

    Created flashcard class and subsequent getter and setter method

--- Flashcards/src/linklist/flashcard.java
+++ Flashcards/src/linklist/flashcard.java
@@ -1,10 +1,18 @@
 package linklist;
 
-// Class untuk node linkedlist pada program utama
-// Mengandung 
+/* 
+ *   Class untuk node linkedlist pada program utama
+ *   Diciptakan untuk membuat circular doubly linked list
+ *   Ada dua field String untuk Kata dan juga Definisi dari kata tersebut
+ *   Lalu dua field lagi untuk flashcard setelah dan juga sebelumnya
+*/
+
+
 public class flashcard {
     private String Kata;
     private String Definisi;
+    private flashcard flashcardSebelum;
+    private flashcard flashcardSetelah;
 
     public String getKata(){
         return Kata;
@@ -12,5 +20,39 @@ public class flashcard {
 
     public String getDefinisi(){
         return Definisi;
+    }
+
+    public void setKata(String Kata){
+        this.Kata = Kata;
+    }
+
+    public void setDefinisi(String Definisi){
+        this.Definisi = Definisi;
+    }
+
+    public flashcard getflashcardSebelum() {
+        return flashcardSebelum;
+    }
+
+    public flashcard getflashcardSetelah() {
+        return flashcardSetelah;
+    }
+
+    public void setflashcardSebelum(flashcard flashcardSebelum) {
+        this.flashcardSebelum = flashcardSebelum;
+    }
+
+    public void setflashcardSetelah(flashcard flashcardSetelah) {
+        this.flashcardSetelah = flashcardSetelah;
+    }
+
+    /**
+     * Method untuk menginisialisasi kartu-kartu saat awal program bermulai
+     * @param Kata Array Mengandung kata
+     * @param Definisi Array mengandung definsi untuk kata tersebut
+     * @param Awal 
+     */
+    public void inisialisasiKartuAwal(String[] Kata, String[] Definisi, flashcard Awal, flashcard Akhir) {
+
     }
 }
